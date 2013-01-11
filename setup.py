d11e1e6bfc505f5a8dee3463f1c48b4e5b240575
Romain Janvier <romain.janvier@hotmail.fr>
2013-01-11 10:28:00 +0100

    more scaffolding

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(name='img2dcm-gui',
       version='0.1-snapshot',
       description='A simple GUI for img2dcm',
       author='The Archeos Team',
+      licence='GPL-2',
       url='https://github.com/archeos/img2dcm-gui',
+      scripts=['img2dcm-gui'], 
       requires=['pyqt (>= 4.9.3)', 'img2dcm (>= 0.1.0)'],
-      licence='GPL-2'
      )
 
